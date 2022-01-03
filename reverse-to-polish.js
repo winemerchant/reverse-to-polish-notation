2335f2681d3acf84bb8328aa157b932338fcecc4
Anastasiia Kozhara <94788527+winemerchant@users.noreply.github.com>
2022-01-03 22:58:54 +0300

    Add files via upload

---
+++ reverse-to-polish.js
@@ -0,0 +1,31 @@
+const Operators = {
+    ['+']: {prioryty: 1, func: (a,b) => a + b},
+    ['-']: {prioryty: 1, func: (a,b) => a - b},
+    ['*']: {prioryty: 2, func: (a,b) => a * b},
+   ['/']: {prioryty: 2, func: (a,b) => a / b},
+}
+
+let OperatorsArr = [] // стек с операторами
+let NumsOutput = [] // массив строки вывода
+
+function reverseToPolish(inputString) { // принимает строку пользователя, возвращает массив в ОПЗ
+    inputString.split('').forEach(element => {
+        if(element in Operators) {
+            while (OperatorsArr.length != 0 && Operators[OperatorsArr[0]]['prioryty'] >= Operators[element]['prioryty']) {
+                NumsOutput.push(OperatorsArr.shift()) 
+
+            }
+            OperatorsArr.unshift(element)
+        } else {
+            NumsOutput.push(element)
+        }   
+    });
+
+    while (OperatorsArr.length != 0) {
+        NumsOutput.push(OperatorsArr.shift())
+    }
+    return NumsOutput
+}
+
+let inputString = prompt()
+alert(reverseToPolish(inputString))
